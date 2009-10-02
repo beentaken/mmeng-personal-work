8905dd901f96cad41841c1265abfba495595931b
amackenzie <amackenzie@chaos.broken-mirror.net>
2009-10-02 11:37:29 -0700

    Initial program.
    numbers.c in some sort of working condition.

--- CS120/lab/02/numbers.c
+++ CS120/lab/02/numbers.c
@@ -1,9 +1,28 @@
-
 #include <stdio.h>
 #include <math.h>
 
 int main(void)
 {
+	float number1, number2, number3;
+	
+	printf("Enter 3 numbers between 0 and 9.999: \n");
+	scanf("%f %f %f", &number1, &number2, &number3);
+
+	printf("Number\tsin\tcos\ttan\tatan\n");
+	printf("-------------------------------------------\n");
+
+	printf("%.5f\t%.3f\t%.3f\t%.3f\t%.3f\n",
+		number1, sin(number1), cos(number1),
+		tan(number1), atan(number1));
+	
+	printf("%.5f\t%.3f\t%.3f\t%.3f\t%.3f\n",
+		number2, sin(number2), cos(number2),
+		tan(number2), atan(number2));
+	
+	printf("%.5f\t%.3f\t%.3f\t%.3f\t%.3f\n",
+		number3, sin(number3), cos(number3),
+		tan(number3), atan(number3));
+
         return(0);
 }
 

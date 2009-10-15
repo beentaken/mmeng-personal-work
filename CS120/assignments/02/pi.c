246b23c80b5a0f33c28fa3cc9bbd7c07d88baf9b
amackenzie <amackenzie@chaos.broken-mirror.net>
2009-10-15 13:48:56 -0700

    Added proper commenting to pi.c

--- CS120/assignments/02/pi.c
+++ CS120/assignments/02/pi.c
@@ -13,35 +13,45 @@ Brief Description:
   method.
 *****************************************************************************/
 
-#include <math.h> /* Need square root function. */
+#include <math.h> /* sqrt */
 
 /* The radius of the circle we are using for the approximation is always 1.0.
  */
 #define RADIUS 1.0
 
-/* Calculates the height of a right-angle triangle given two measurements.
+/*****************************************************************************
+ *    Function: height
  *
- * base^2 + height^2 = hypotenuse^2, so height can be solved for.
- */
+ * Description: Finds the height of a right-angle triangle given the length
+ *              of the base and hypotenuse.
+ *
+ *      Inputs: base - length of the base.
+ *              hypotenuse - length of the hypotenuse.
+ *
+ *     Outputs: The appropriate height for the triangle.
+ ****************************************************************************/
 double height(double base, double hypotenuse)
 {
     return(sqrt(hypotenuse*hypotenuse - base*base));
 }
 
-/* Approximates pi using the area of a circle.
+/*****************************************************************************
+ *    Function: circle_pi
  *
- * Compute the area with a sort of Riemann Sum, then solve for pi using the
- * equation A = pi * r^2.
+ * Description: Approximates pi using the area of a circle formula.
  *
- * We are using the midpoint of the top of the rectangle for the
- * approximation.
+ *      Inputs: rectangles - number of rectangles to use in the approximation
+ *                           of the area under the quarter-circle.
  *
- * Note that we're only calculating a quarter circle's area. Final area
- * needs to be multiplied by 4.
- */
+ *     Outputs: An approximate value of pi.
+ ****************************************************************************/
 double circle_pi(int rectangles)
 {
+	/* Iteration counter for the 'for' loop. */
     int i;
+
+	/* sum accumulates the areas of the rectangles in the loop.
+	 * width is the base length of each rectangle. */
 	double sum = 0.0, width = RADIUS/rectangles;
 
 	for (i = 0; i < rectangles; ++i)
@@ -55,31 +65,43 @@ double circle_pi(int rectangles)
     return(4.0 * sum);
 }
 
-/* Approximates pi using Leibniz's method.
+/*****************************************************************************
+ *    Function: leibniz_pi
  *
- * General series is pi/4 = 1 - 1/3 + 1/5 - 1/7...
- */
+ * Description: Approximates pi using Leibniz's method.
+ *              General series is pi/4 = 1 - 1/3 + 1/5 - 1/7...
+ *
+ *      Inputs: iterations - number of terms to calculate in the series.
+ *
+ *     Outputs: An approximate value for pi.
+ ****************************************************************************/
 double leibniz_pi(int iterations)
 {
+	/* First term is 1/1. */
 	int denominator = 1;
+
+	/* Accumulate the approximation in sum. */
 	double sum = 0.0;
 
-	while (denominator < iterations*2)
+	while (denominator < iterations * 2)
 	{
 		/* denominator is always twice as large as the number of iterations
-		 * we've gone through, so make sure to cut it in half. */
-	    if ((denominator/2) % 2 == 0)
+		 * we've gone through, so make sure to cut it in half before testing
+		 * whether we're on an even or odd iteration. */
+	    if ((denominator / 2) % 2 == 0)
 		{
 	        sum += 1.0 / denominator;
 	    }
 		else
 		{
-		    sum += -1.0 / denominator;
+		    sum -= 1.0 / denominator;
 		}
 		
 		denominator += 2;
 	}
 
+	/* Formula approximates pi/4, so make sure to scale the value back up
+	 * when it's returned. */
     return(sum * 4);
 }
 

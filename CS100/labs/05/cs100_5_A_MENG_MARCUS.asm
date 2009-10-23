766449d3780d199cf6bf515424546a3867b21160
marcus.meng <marcus.meng@dit864.digipen.edu>
2009-10-23 10:36:27 -0700

    Completed multiplier.

--- CS100/labs/05/cs100_5_A_MENG_MARCUS.asm
+++ CS100/labs/05/cs100_5_A_MENG_MARCUS.asm
@@ -1,8 +1,8 @@
 ; CS 100 Assignment 5
 ; Due Date: Friday 10/24/2008 @ 12:00 noon
 ; Student Name:	Marcus Meng
-; Student ID#: 
-; Section: 
+; Student ID#: 80009207
+; Section: A
 ; 
 TITLE 		"CS 100 Assignment 5"
 LIST		P=18F452
@@ -34,6 +34,15 @@ ORG	0x18
 ;         
 ORG	0x040   ; Set program memory at any address beyond the needs of interrupt service routines  
 
+; NOTES
+
+; RESULT 	+1		+2	 	+3
+; L*L:L		L*L:H	H*H:L	H*H:H
+;			H*L:L	Carry	Carry
+;					H*L:H	Carry
+;			L*H:L	Carry	Carry
+;					L*H:H	Carry
+
 start         
  MOVLW num1_h ; Set up NUM1 to represent the full number.
  MOVWF NUM1+1
@@ -50,9 +59,27 @@ start
  MOVFF PRODL, RESULT
  MOVFF PRODH, RESULT+1		
 
- MOVF NUM1+1, W
+ MOVF NUM1+1, W			; Multiply low bits and store into lower registers.
  MULWF NUM2+1
  MOVFF PRODL, RESULT+2
  MOVFF PRODH, RESULT+3
+
+ MOVF NUM1+1, W			; First take care of HxL * 10^8
+ MULWF NUM2
+ MOVF PRODL, W
+ ADDWF RESULT+1, F		; Add to lower middle.
+ MOVF PRODH, W
+ ADDWFC RESULT+2, F		; Add to upper middle and take care of the carry.
+ CLRF WREG
+ ADDWFC RESULT+3, F		; Just in case upper add also carried.
+
+ MOVF NUM1, W			; Now to deal with LxH * 10^8
+ MULWF NUM2+1
+ MOVF PRODL, W
+ ADDWF RESULT+1, F		; Lower middle.
+ MOVF PRODH, W
+ ADDWFC RESULT+2, F		; Upper middle, with carry.
+ CLRF WREG
+ ADDWFC RESULT+3, F		; Fix carry.
 
 END

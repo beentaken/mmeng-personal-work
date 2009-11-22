ad3309624055e7a41170e251e41d72fb21d1d71f
Marcus Meng <aenslad.mackenzie@gmail.com>
2009-11-22 12:51:00 -0800

    Modified CS120 Lab 09 to handle multiple files.

--- CS120/lab/09/main.c
+++ CS120/lab/09/main.c
@@ -5,13 +5,18 @@ void dump(const char *filename);
 
 int main(int argc, char **argv)
 {
+	int i; /* Loop counter. */
+
 	if (argc < 2)
 	{
 		printf("Usage: %s filename\n", argv[0]);
 		return -1;
 	}
 
-	dump(argv[1]);
+	for ( i = 1; i < argc; ++i )
+	{
+		dump(argv[i]);
+	}
 
 	 return 0;
 }

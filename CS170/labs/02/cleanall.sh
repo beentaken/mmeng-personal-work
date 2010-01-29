18b5e187aa15b39528ab332931cf4e952fe82b54
Marcus Meng <aenslad.mackenzie@gmail.com>
2010-01-29 13:02:25 -0800

    Slight modifications.

---
+++ CS170/labs/02/cleanall.sh
@@ -0,0 +1,4 @@
+/cygdrive/c/cygwin/bin/make CXX=g++ clean
+/cygdrive/c/cygwin/bin/make CXX=cl clean
+/cygdrive/c/cygwin/bin/make CXX=bcc32 clean
+
---
+++ CS170/labs/02/makeall.sh
@@ -0,0 +1,4 @@
+/cygdrive/c/cygwin/bin/make CXX=g++
+/cygdrive/c/cygwin/bin/make CXX=cl
+/cygdrive/c/cygwin/bin/make CXX=bcc32
+

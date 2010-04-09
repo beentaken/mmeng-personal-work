cfdc91457d642a6a11848855a4cdd416a85e0157
Marcus Meng <aenslad.mackenzie@gmail.com>
2010-04-09 02:30:20 -0700

    Completed commenting.

--- CS170/labs/09/Vector.cpp
+++ CS170/labs/09/Vector.cpp
@@ -1,4 +1,24 @@
-/* file_header_comment */
+/*****************************************************************************/
+/*!
+\file   filename
+\author Marcus Meng
+\par    email: marcus.meng@digipen.edu
+\par    DigiPen login: marcus.meng
+\par    Course: CS170
+\par    Lab 9
+\date   April 9, 2010
+\brief
+	This file contains the implementations for the various Vector member
+	functions.
+
+Hours spent on this assignment: 1
+
+Specific portions that gave you the most trouble:
+
+Figuring out when to use grow() and when to manually allocate stuff.
+
+*/
+/*****************************************************************************/
 
 #include <iostream>  // cout
 #include <cstdlib>   // abort
@@ -7,12 +27,28 @@
 namespace CS170
 {
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Default constructor.
+*/
+/*****************************************************************************/
 Vector::Vector(void) : array_(0), size_(0), capacity_(0), allocs_(0)
 {
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Constructs a new Vector from the given array of integers.
+
+\param array
+	The array containing the data to initialize the Vector to.
+
+\param size
+	The size of the array.
+*/
+/*****************************************************************************/
 Vector::Vector(const int *array, unsigned size)
 :array_(NULL), size_(0), capacity_(0), allocs_(0)
 {
@@ -24,26 +60,58 @@ Vector::Vector(const int *array, unsigned size)
 	size_ = size;
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Copy constructor.
+
+\param rhs
+	The Vector containing the data to initialize the current object to.
+
+\return
+	Return_desc
+*/
+/*****************************************************************************/
 Vector::Vector(const Vector &rhs)
 :array_(NULL), size_(0), capacity_(0), allocs_(0)
 {
 	(*this) = rhs;
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Destructor.
+*/
+/*****************************************************************************/
 Vector::~Vector(void)
 {
 	clear();
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Finds whether the current Vector is empty.
+
+\return
+	True if the current Vector is empty, false otherwise.
+*/
+/*****************************************************************************/
 bool Vector::empty(void) const
 {
   return size_ == 0;
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Appends the given value to the Vector.
+
+\param value
+	The value to append to the Vector.
+*/
+/*****************************************************************************/
 void Vector::push_back(int value)
 {
 	if (size() >= capacity())
@@ -55,13 +123,26 @@ void Vector::push_back(int value)
 	++size_;
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Prepends a value to the current Vector.
+
+\param value
+	The value to prepend.
+*/
+/*****************************************************************************/
 void Vector::push_front(int value)
 {
 	insert(value, 0);
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Removes the last value in the Vector.
+*/
+/*****************************************************************************/
 void Vector::pop_back(void)
 {
 	if (!empty())
@@ -70,14 +151,30 @@ void Vector::pop_back(void)
 	}
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Removes the first value in the vector.
+*/
+/*****************************************************************************/
 void Vector::pop_front(void)
 {
 	if (!empty())
 		remove(array_[0]);
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Inserts a given value into the index provided.
+
+\param value
+	The value to insert.
+
+\param position
+	The index to insert the new value at.
+*/
+/*****************************************************************************/
 void Vector::insert(int value, unsigned position)
 {
 	if (size() >= capacity())
@@ -90,7 +187,15 @@ void Vector::insert(int value, unsigned position)
 	++size_;
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Removes the first occurance of a given value from the array.
+
+\param value
+	The value to remove.
+*/
+/*****************************************************************************/
 void Vector::remove(int value)
 {
 	if (empty())
@@ -102,7 +207,18 @@ void Vector::remove(int value)
 	--size_;
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Finds the index of the first occurance of the given value.
+
+\param value
+	The value to locate.
+
+\return
+	The index of the value provided, -1 if the value was not found.
+*/
+/*****************************************************************************/
 int Vector::find(int value) const
 {
 	for (unsigned i = 0; i < size(); ++i)
@@ -116,7 +232,13 @@ int Vector::find(int value) const
 	return(-1); // I DUN CAN FIND NUMBAH.
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Reallocates the capacity of the current Vector to match the actual
+	size of the vector.
+*/
+/*****************************************************************************/
 void Vector::pack(void)
 {
 	if (capacity() > size())
@@ -135,7 +257,13 @@ void Vector::pack(void)
 	}
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Deallocates the current array and sets the various helper variables
+	to a default.
+*/
+/*****************************************************************************/
 void Vector::clear(void)
 {
 	delete[] array_;
@@ -143,25 +271,60 @@ void Vector::clear(void)
 	size_ = capacity_ = 0;
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Finds the number of existing elements in the current Vector.
+
+\return
+	The number of elements in the Vector.
+*/
+/*****************************************************************************/
 unsigned Vector::size(void) const
 {
   return size_;
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Finds the amount of units allocated in the current Vector.
+
+\return
+	The number of units allocated.
+*/
+/*****************************************************************************/
 unsigned Vector::capacity(void) const
 {
   return capacity_;
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Finds the number of memory allocations performed by this object.
+
+\return
+	The number of memory allocations performed.
+*/
+/*****************************************************************************/
 unsigned Vector::allocations(void) const
 {
   return allocs_;
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Overloaded assignment operator.
+
+\param rhs
+	The Vector containing data to assign to the current Vector.
+
+\return
+	A reference to the current Vector.
+*/
+/*****************************************************************************/
 Vector& Vector::operator=(const Vector& rhs)
 {
 	if (this != &rhs)
@@ -182,7 +345,18 @@ Vector& Vector::operator=(const Vector& rhs)
 	return(*this);
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Overloaded add-and-assign operator.
+
+\param rhs
+	The Vector containing the data to append to the current Vector.
+
+\return
+	A reference to the current Vector.
+*/
+/*****************************************************************************/
 Vector& Vector::operator+=(const Vector& rhs)
 {
 	for (unsigned i = 0; i < rhs.size(); ++i)
@@ -193,7 +367,18 @@ Vector& Vector::operator+=(const Vector& rhs)
 	return(*this);
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Overloaded addition operator.
+
+\param rhs
+	The Vector containing information to append.
+
+\return
+	A new Vector containing the contents of the current and rhs Vectors.
+*/
+/*****************************************************************************/
 Vector Vector::operator+(const Vector& rhs) const
 {
 	Vector temp(*this);
@@ -202,14 +387,36 @@ Vector Vector::operator+(const Vector& rhs) const
 	return(temp);
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Overloaded index operator (const version).
+
+\param index
+	The index of the value to obtain.
+
+\return
+	A copy of the contents at that index.
+*/
+/*****************************************************************************/
 int Vector::operator[](unsigned index) const
 {
 	check_bounds(index);
 	return(array_[index]);
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Overloaded index operator.
+
+\param index
+	The index of the value to get.
+
+\return
+	A reference to the value at the specified index.
+*/
+/*****************************************************************************/
 int& Vector::operator[](unsigned index)
 {
 	check_bounds(index);
@@ -222,7 +429,16 @@ int& Vector::operator[](unsigned index)
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Checks whether the specified index is valid within the current
+	Vector.
+
+\param index
+	The index to check.
+*/
+/*****************************************************************************/
 void Vector::check_bounds(unsigned index) const
 {
     // Don't have to check for < 0 because index is unsigned
@@ -234,7 +450,12 @@ void Vector::check_bounds(unsigned index) const
   }
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Doubles the capacity of the internally allocated array.
+*/
+/*****************************************************************************/
 void Vector::grow(void)
 {
 	// Double the capacity
@@ -252,7 +473,21 @@ void Vector::grow(void)
 	++allocs_;
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Copies the contents of a source array into a destination array.
+
+\param source
+	A pointer to the start of the array to copy from
+
+\param size
+	The number of elements to copy from the source.
+
+\param destination
+	A pointer to the start of the array to copy to.
+*/
+/*****************************************************************************/
 void Vector::copy_array(const int *source, const unsigned size, int *destination)
 {
 	for (unsigned i = 0; i < size; ++i)
@@ -261,7 +496,22 @@ void Vector::copy_array(const int *source, const unsigned size, int *destination
 	}
 }
 
-/* function_header_comment */
+/*****************************************************************************/
+/*!
+\brief
+	Copies the contents of a source array into a destination array
+	starting from the last sequential value.
+
+\param source
+	The source array to copy from.
+
+\param size
+	The number of elements to copy from the source.
+
+\param destination
+	A pointer to the start of the array to copy to.
+*/
+/*****************************************************************************/
 void Vector::reverse_copy_array(const int *source, const unsigned size, int *destination)
 {
 	for (int i = static_cast<int>(size); i >= 0; --i)
